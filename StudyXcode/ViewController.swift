8f76c89fa9c97729de29ae91a6e63699475156cb
growingdev707 <growingdev707@gmail.com>
2023-06-03 15:43:50 +0900

    SecondViewController 생성, button Action으로 화면전환 구현

--- StudyXcode/ViewController.swift
+++ StudyXcode/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var centerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        centerButton.setTitle("Center Button", for: .normal)
+        centerButton.tintColor = .white
     }
 
+    @IBAction func centerButtonTap(_ sender: Any) {
+        print("Tap")
+        let secondViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondViewController")
+        self.navigationController?.pushViewController(secondViewController, animated: true)
+    }
 
 }
 
